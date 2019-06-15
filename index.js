f767478b8cc9c4c7cc4b24a6072fbd8d69b66fa4
Cris Mihalache <me@f3rno.com>
2019-06-15 22:29:53 +0700

    (initial commit)

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const debug = require('debug')('bfx:hf:models:adapter-lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const _isString = require('lodash/isString')
+const _isObject = require('lodash/isObject')
+const _isEmpty = require('lodash/isEmpty')
+const lowdb = require('lowdb')
+
+const mapMethods = require('./lib/map_methods')
+const collectionMethods = require('./lib/collection_methods')
+
+module.exports = ({
+  dbPath,
+  defaultData,
+  backend = FileSync,
+}) => {
+  if (!_isString(dbPath) || _isEmpty(dbPath)) {
+    throw new Error('DB path required')
+  }
+
+  debug('loading from %s', dbPath)
+
+  const db = lowdb(new backend(dbPath))
+
+  if (_isObject(defaultData) && !_isEmpty(defaultData)) {
+    db.defaults(defaultData).write()
+  }
+
+  return {
+    db,
+    mapMethods,
+    collectionMethods,
+  }
+}
